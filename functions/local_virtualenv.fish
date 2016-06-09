e42b204019e004a32c3d51448ad7094c01dbb5de
John Leith <jleith@velocitywebworks.com>
2016-06-09 09:19:18 -0600

    testing

--- functions/local_virtualenv.fish
+++ functions/local_virtualenv.fish
@@ -1,4 +1,16 @@
 function local_virtualenv -d "My package" --on-variable PWD --description "auto-activate local virtualenvs"
   # Package entry-point
   echo "hey"
+  if test -e $(ls venv-*/bin/activate 2>/dev/null)
+    if ! test -n $VIRTUAL_ENV
+      export _VENV_NAME=
+    end
+    
+    # Check to see if already activated to avoid redundant activating
+    if [ "$_VENV_NAME" != "$(basename `pwd`)" ]; then
+      export _VENV_NAME=$(basename `pwd`)
+      echo Activating virtualenv \"$_VENV_NAME\"...
+      source ./venv-*/bin/activate
+    end
+  end
 end
